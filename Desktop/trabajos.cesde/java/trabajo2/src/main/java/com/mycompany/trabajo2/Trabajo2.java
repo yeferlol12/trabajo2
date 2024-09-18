d28863deed0bfebefdeaf27cd437eed9f8686f03
yeferlol12 <ysantamaria@cesde.net>
2024-09-17 22:42:58 -0500

    1

---
+++ Desktop/trabajos.cesde/java/trabajo2/src/main/java/com/mycompany/trabajo2/Trabajo2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo2;
+
+/**
+ *
+ * @author SANTA
+ */
+public class Trabajo2 {
+
+    public static void main(String[] args) {
+        
+        int n = 16 ;
+        int s = 12;
+        int ct = n - s ;
+        
+        System.out.println(" niños que quedaron " + ct );
+    }
+}
